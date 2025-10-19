d6192dbf9c796f40cb58cac24977d4940e7ebcb5
SaraTohidian <sara.tohidian@DESKTOP-CMIQN01>
2025-10-19 17:17:08 +0330

    initial commit

---
+++ Services/Catalog/Catalog.Api/Context/CatalogContext.cs
@@ -0,0 +1,16 @@
+﻿using Catalog.Api.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.Api.Context
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public IMongoCollection<Product> Products { get; }
+        public CatalogContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.GetValue<string>("ConnectionSettings:ConnectionString"));
+            var database = client.GetDatabase(configuration.GetValue<string>("ConnectionSettings:DatabaseName"));
+            Products = database.GetCollection<Product>(configuration.GetValue<string>("ConnectionSettings:CollectionName"));
+        }
+    }
+}
